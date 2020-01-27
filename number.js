6ebe5360241958859e2aee1bf46d62c76cfa5a31
Clint <Ccrockett154@west-mec.org>
2020-01-27 13:48:42 -0700

    numbers project almost done

--- number.js
+++ number.js
@@ -188,9 +188,76 @@ function rangeM(){
 }
 
 //ex there are 2 2
-// function duplicateM(){
+function duplicateM(){
+    var first = parseFloat(doc1.value);
+    var second = parseFloat(doc2.value);
+    var third = parseFloat(doc3.value);
+    var fourth = parseFloat(doc4.value);
+    var fifth = parseFloat(doc5.value);
+    var sixth = parseFloat(doc6.value);
 
-// }
+    var o1 = 0;
+    var o2 = 0;
+    var o3 = 0;
+    var o4 = 0;
+    var o5 = 0;
+    var o6 = 0;
+
+    //this will check if the first number has any duplicates
+    if(first===second){
+        o1++;
+    }
+    if(first===third){
+        o1++;
+    }
+    if(first===fourth){
+        o1++;
+    }
+    if(first===fifth){
+        o1++;
+    }
+    if(first===sixth){
+        o1++;
+    }
+    //this will check if the second number has any duplicates
+    if(second===third){
+        o2++;
+    }
+    if(second===fourth){
+        o2++;
+    }
+    if(second===fifth){
+        o2++;
+    }
+    if(second===sixth){
+        o2++;
+    }
+    //this will check if the third number has any duplicates
+    if(third===fourth){
+        o3++;
+    }
+    if(third===fifth){
+        o3++;
+    }
+    if(third===sixth){
+        o3++;
+    }
+    //this will check if the fourth number has any duplicates
+
+    if(fourth===fifth){
+        o4++;
+    }
+    if(fourth===sixth){
+        o4++;
+    }
+    //this will check if the fifth number has any duplicates
+    if(fifth===sixth){
+        o5++;
+    }
+    // you dont need to check the sixth number because you already did
+
+    output.innerHTML = o1 + ", " + o2 + ", " + o3 + ", " + o4 + ", " + o5;
+}
 
 //numbers that are not simaler
 function uniqueM(){
@@ -208,36 +275,92 @@ function uniqueM(){
     var o5 = 0;
     var o6 = 0;
 
-    if(first !== second && first !== third && first !== fourth && first !== fifth && first !== sixth){
-    output.innerHTML = first + ", " + second + ", " + third + ", " + fourth + ", " + fifth + ", " + sixth;;
+    //this will check if the first number has any duplicates
+    if(first===second){
+        o1++;
     }
-    else if(first === second && first !== third && first !== fourth && first !== fifth && first !== sixth){
-        output.innerHTML = first + ", " + third + ", " + fourth + ", " + fifth + ", " + sixth;
+    if(first===third){
+        o1++;
     }
-    if(first !== second && first === third && first !== fourth && first !== fifth && first !== sixth){
-        output.innerHTML = first + ", " + second + ", " + fourth + ", " + fifth + ", " + sixth;;
+    if(first===fourth){
+        o1++;
     }
-    if(first !== second && first !== third && first === fourth && first !== fifth && first !== sixth){
-        output.innerHTML = first + ", " + second + ", " + third + ", " + fifth + ", " + sixth;;
+    if(first===fifth){
+        o1++;
     }
-    if(first !== second && first !== third && first !== fourth && first === fifth && first !== sixth){
-        output.innerHTML = first + ", " + second + ", " + third + ", " + fourth + ", " + sixth;;
+    if(first===sixth){
+        o1++;
     }
-    else if(first === second && first !== third && first !== fourth && first !== fifth && first === sixth){
-        output.innerHTML = first + ", " + third + ", " + fourth + ", " + fifth;
+    //this will check if the second number has any duplicates
+    if(second===third){
+        o2++;
     }
+    if(second===fourth){
+        o2++;
+    }
+    if(second===fifth){
+        o2++;
+    }
+    if(second===sixth){
+        o2++;
+    }
+    //this will check if the third number has any duplicates
+    if(third===fourth){
+        o3++;
+    }
+    if(third===fifth){
+        o3++;
+    }
+    if(third===sixth){
+        o3++;
+    }
+    //this will check if the fourth number has any duplicates
+
+    if(fourth===fifth){
+        o4++;
+    }
+    if(fourth===sixth){
+        o4++;
+    }
+    //this will check if the fifth number has any duplicates
+    if(fifth===sixth){
+        o5++;
+    }
+    // you dont need to check the sixth number because you already did
+
+    output.innerHTML = o1 + ", " + o2 + ", " + o3 + ", " + o4 + ", " + o5;
 
 
-    else if(first === second && first !== third && first !== fourth && first !== fifth && first !== sixth){
-        output.innerHTML = first + ", " + third + ", " + fourth + ", " + fifth + ", " + sixth;
-    }
-    else if(first === second && first !== third && first !== fourth && first !== fifth && first !== sixth){
-        output.innerHTML = first + ", " + third + ", " + fourth + ", " + fifth + ", " + sixth;
-    }
-    else if(first === second && first !== third && first !== fourth && first !== fifth && first !== sixth){
-        output.innerHTML = first + ", " + third + ", " + fourth + ", " + fifth + ", " + sixth;
-    }
-    else if(first === second && first !== third && first !== fourth && first !== fifth && first !== sixth){
-        output.innerHTML = first + ", " + third + ", " + fourth + ", " + fifth + ", " + sixth;
-    }
+    //this was me trying
+
+    // if(first !== second && first !== third && first !== fourth && first !== fifth && first !== sixth){
+    // output.innerHTML = first + ", " + second + ", " + third + ", " + fourth + ", " + fifth + ", " + sixth;;
+    // }
+    // else if(first === second && first !== third && first !== fourth && first !== fifth && first !== sixth){
+    //     output.innerHTML = first + ", " + third + ", " + fourth + ", " + fifth + ", " + sixth;
+    // }
+    // if(first !== second && first === third && first !== fourth && first !== fifth && first !== sixth){
+    //     output.innerHTML = first + ", " + second + ", " + fourth + ", " + fifth + ", " + sixth;;
+    // }
+    // if(first !== second && first !== third && first === fourth && first !== fifth && first !== sixth){
+    //     output.innerHTML = first + ", " + second + ", " + third + ", " + fifth + ", " + sixth;;
+    // }
+    // if(first !== second && first !== third && first !== fourth && first === fifth && first !== sixth){
+    //     output.innerHTML = first + ", " + second + ", " + third + ", " + fourth + ", " + sixth;;
+    // }
+    // else if(first === second && first !== third && first !== fourth && first !== fifth && first === sixth){
+    //     output.innerHTML = first + ", " + third + ", " + fourth + ", " + fifth;
+    // }
+    // else if(first === second && first !== third && first !== fourth && first !== fifth && first !== sixth){
+    //     output.innerHTML = first + ", " + third + ", " + fourth + ", " + fifth + ", " + sixth;
+    // }
+    // else if(first === second && first !== third && first !== fourth && first !== fifth && first !== sixth){
+    //     output.innerHTML = first + ", " + third + ", " + fourth + ", " + fifth + ", " + sixth;
+    // }
+    // else if(first === second && first !== third && first !== fourth && first !== fifth && first !== sixth){
+    //     output.innerHTML = first + ", " + third + ", " + fourth + ", " + fifth + ", " + sixth;
+    // }
+    // else if(first === second && first !== third && first !== fourth && first !== fifth && first !== sixth){
+    //     output.innerHTML = first + ", " + third + ", " + fourth + ", " + fifth + ", " + sixth;
+    // }
 }
